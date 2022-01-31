f103639eb0da16fce3227e90a73d8858055f9d33
Elaina-Alex <luanyin4444@qq.com>
2022-01-31 12:39:35 +0800

    ~

--- API/UrlConstants.py
+++ API/UrlConstants.py
@@ -1,6 +1,6 @@
 WEB_SITE = "http://api.aixdzs.com/"
 TAG_API = "book-sort?gender={}&type=hot&major={}&minor=&start={}&limit=20"
-BOOK_INFO_API = '/book/{}'
+BOOK_INFO_API = 'book/{}'
 BOOK_CATALOGUE = 'content/{}?view=chapter'
 GET_TYPE_INFO = 'sort/lv2'
 CHAPTER_API = 'chapter/{}'
